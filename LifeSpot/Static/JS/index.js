cd6f125ff8633d7213f73ae5794e9b8bbac71752
Константин Силенков <silenkov@inbox.ru>
2023-08-16 16:56:32 +0500

    Модуль 30

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -12,32 +12,16 @@
 }
 
 /*
-* Сессия теперь создается в общей области видимости.
-* Будет "захватываться" тремя функциями
-* 
-* */
-let session = new Map();
-
-/*
-* Сохранение данных сессии сразу при заходе пользователя на страницу
-* 
-* */
-function handleSession() {
-    // Сохраним время начала сессии
-    session.set("startDate", new Date().toLocaleString())
-    // Сохраним UserAgent
-    session.set("userAgent", window.navigator.userAgent)
-}
-
-/*
 * Проверка возраста пользователя
-* 
+*
 * */
-function checkAge() {
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
-
-    if (session.get("age") >= 18) {
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+let checker = function (newVisit) {
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        // Добавим проверку на первое посещение, чтобы не показывать приветствие
+        // лишний раз
+        if (newVisit) {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
     }
     else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
@@ -47,12 +31,48 @@ function checkAge() {
 
 /*
 * Вывод данных сессии в консоль
-* 
+*
 * */
-let sessionLog = function logSession() {
-    for (let result of session) {
-        console.log(result)
+let logger = function () {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate"))
+    console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent"))
+    console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"))
+}
+
+/*
+* Сохранение данных сессии сразу при заходе пользователя на страницу
+*
+* */
+function handleSession(logger, checker) {
+
+    // Проверяем дату захода и проставляем, если новый визит
+    if (window.sessionStorage.getItem("startDate") == null) {
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
     }
+
+    // Проверяем userAgent и проставляем, если новый визит
+    if (window.sessionStorage.getItem("userAgent") == null) {
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    }
+
+    // Проверяем возраст и проставляем, если новый визит
+    if (window.sessionStorage.getItem("userAge") == null) {
+        let input = prompt("Пожалуйста, введите ваш возраст?");
+        window.sessionStorage.setItem("userAge", input)
+
+        /* Возраст отсутствовал в sessionStorage. Значит, это первый визит пользователя, и
+         при прохождении проверки на возраст он увидит приветствие*/
+        checker(true)
+    } else {
+
+        /* Пользователь заходит не первый раз, приветствие не показываем. */
+        checker(false)
+    }
+
+    /* Вызываем переданную в качестве колл-бэка функцию логирования.
+        передавать в качестве коллбека не обязательно, можно вызвать и напрямую, но мы добавили для повторения.
+    */
+    logger()
 }
 
 /*
@@ -62,42 +82,3 @@ let sessionLog = function logSession() {
 setTimeout(() =>
     alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
     1000);
-
-/*
-* Запросим пользовательский ввод
-* и сохраним отзыв в объект
-*
-* */
-function getReview() {
-    // Создадим объект
-    let review = {}
-
-    // Сохраним свойство имени
-    review["userName"] = prompt("Как вас зовут ?")
-    if (review["userName"] == null) {
-        return
-    }
-
-    // Сохраним текст отзыва
-    review["comment"] = prompt("Напишите свой отзыв")
-    if (review["comment"] == null) {
-        return
-    }
-
-    // Сохраним текущее время
-    review["date"] = new Date().toLocaleString()
-
-    // Добавим на страницу
-    writeReview(review)
-}
-
-/*
-* Запишем отзыв на страницу
-*
-* */
-const writeReview = review => {
-    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
-        `<p>${review['comment']}</p>` +
-        '</div>';
-}
